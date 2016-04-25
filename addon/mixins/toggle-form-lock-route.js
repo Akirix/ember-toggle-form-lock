4c17889db7c74b5654b5d2cd90384d457024e3b2
Daniel Breen <daninreallife7@gmail.com>
2016-04-25 09:33:15 -0600

    add resetController event to toggle-form-lock-route mixin

--- addon/mixins/toggle-form-lock-route.js
+++ addon/mixins/toggle-form-lock-route.js
@@ -4,6 +4,10 @@ export default Ember.Mixin.create( {
 
     setupController: function( controller ){
         controller.set( 'formIsLocked', true );
+    },
+
+    resetController: function( controller ){
+        controller.set( 'formIsLocked', true );
     }
 
 } );
